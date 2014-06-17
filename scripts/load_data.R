9338554998c1606060bdf586a165bafcb6f2a92e
BernhardKonrad <bernhard.konrad@gmail.com>
2014-06-17 13:39:11 -0700

    This add an instruction on how to source the load_data file

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -1,3 +1,7 @@
 #helper script to load my data
+#call this file with
+# source(file.path("scripts", "load_data"))
+#in order to load the gapminder dataset
+
 gDat <- read.delim(file.path("data",
                              "2014-06-16", "gapminder", "gapminderDataFiveYear.txt"))
